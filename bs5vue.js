d360232091237aac9e9fbcbfd364d86030352047
Laiii1017 <145099992+Laiii1017@users.noreply.github.com>
2025-01-08 01:20:23 +0800

    01074

--- bs5vue.js
+++ bs5vue.js
@@ -1,17 +1,17 @@
-var card = Vue.createApp({
+var Character1Data = Vue.createApp({
     data() {
         return {
-            cards: [], // 初始化 cards 為空陣列
+            Character1: [], // 初始化 cards 為空陣列
         };
     },
     mounted() {
         // 使用 jQuery 的 $.ajax 發送請求
         $.ajax({
-            url: "/profolio", // API 路徑
+            url: "/character1", // API 路徑s
             method: "GET",    // HTTP 方法
             dataType: "json", // 資料格式
             success: (results) => {
-                this.cards = results; // 將回傳的資料綁定到 Vue 的 cards
+                this.Character1 = results; // 將回傳的資料綁定到 Vue 的 cards
             },
             error: (xhr, status, error) => {
                 console.error("Error fetching data:", status, error);
@@ -19,6 +19,56 @@ var card = Vue.createApp({
         });
     },
 });
-
 // 掛載 Vue 應用程式到指定的 DOM 元素
-card.mount("#test");
+Character1Data.mount("#character1");
+
+
+
+var Character2Data = Vue.createApp({
+    data() {
+        return {
+            Character2: [], // 初始化 cards 為空陣列
+        };
+    },
+    mounted() {
+        // 使用 jQuery 的 $.ajax 發送請求
+        $.ajax({
+            url: "/character2", // API 路徑s
+            method: "GET",    // HTTP 方法
+            dataType: "json", // 資料格式
+            success: (results) => {
+                this.Character2 = results; // 將回傳的資料綁定到 Vue 的 cards
+            },
+            error: (xhr, status, error) => {
+                console.error("Error fetching data:", status, error);
+            },
+        });
+    },
+});
+// 掛載 Vue 應用程式到指定的 DOM 元素
+Character2Data.mount("#character2");
+
+
+var Character3Data = Vue.createApp({
+    data() {
+        return {
+            Character3: [], // 初始化 cards 為空陣列
+        };
+    },
+    mounted() {
+        // 使用 jQuery 的 $.ajax 發送請求
+        $.ajax({
+            url: "/character3", // API 路徑s
+            method: "GET",    // HTTP 方法
+            dataType: "json", // 資料格式
+            success: (results) => {
+                this.Character3 = results; // 將回傳的資料綁定到 Vue 的 cards
+            },
+            error: (xhr, status, error) => {
+                console.error("Error fetching data:", status, error);
+            },
+        });
+    },
+});
+// 掛載 Vue 應用程式到指定的 DOM 元素
+Character3Data.mount("#character3");
